db732727f54d30400aa0b039fdb7d5363fbc81bc
Agata <twardowska123@gmail.com>
2018-07-17 20:38:58 +0200

    poprawki

--- src/Program.java
+++ src/Program.java
@@ -5,30 +5,21 @@ public class Program {
 
     public static void main(String[] args) {
 
+        String[] imiona = new String[5];
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Podaj 5 imion:");
-        String imie1 = scan.nextLine();
-        String imie2 = scan.nextLine();
-        String imie3 = scan.nextLine();
-        String imie4 = scan.nextLine();
-        String imie5 = scan.nextLine();
 
-        String[] imiona = new String[5];
-        imiona[0] = "Cześć" + " " + imie1;
-        imiona[1] = "Cześć" + " " + imie2;
-        imiona[2] = "Cześć" + " " + imie3;
-        imiona[3] = "Cześć" + " " + imie4;
-        imiona[4] = "Cześć" + " " + imie5;
+        for (int i = 0; i < imiona.length; i++) {
+            imiona[i] = scan.nextLine();
 
-        for (int i = imiona.length -1; i >= 0 ; --i) {
-            System.out.println(imiona[i]);
-            
         }
-        
+
+        for (int x = imiona.length - 1; x >= 0 ; --x) {
+            System.out.println("Cześć" + " " + imiona[x]);
+
+
+        }
     }
-
-
-
 
 }
